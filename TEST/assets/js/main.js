809bb2d79d85b0d498291894ddd59437432b10d6
EmanZHD <zahidimane1@gmail.com>
2025-03-24 15:04:32 +0000

    fixed

--- TEST/assets/js/main.js
+++ TEST/assets/js/main.js
@@ -1,13 +1,13 @@
 const inputs = document.querySelectorAll(".input");
-
-function addcl(){
+const toggleIcon = document.querySelector(".show-hide")
+function addcl() {
     let parent = this.parentNode.parentNode;
     parent.classList.add("focus");
 }
 
-function remcl(){
+function remcl() {
     let parent = this.parentNode.parentNode;
-    if(this.value == ""){
+    if (this.value == "") {
         parent.classList.remove("focus");
     }
 }
@@ -16,13 +16,26 @@ inputs.forEach(input => {
     input.addEventListener("focus", addcl);
     input.addEventListener("blur", remcl);
 });
+function togglePassword() {
+    const passwordInput = document.getElementById('passwordInput');
+    const eyeIcon = document.querySelector('.show-hide i');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.replace('fa-eye', 'fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.replace('fa-eye-slash', 'fa-eye');
+    }
+}
 function getCurrentYearAsMax() {
     const currentYear = new Date().getFullYear();
     const footer = document.querySelector(".footer-content");
     footer.innerHTML = `
     <p>&copy;${currentYear} 01Oujda-GraphQL | Crafted with ❤ by
-        <a href="https://learn.zone01oujda.ma/git/izahid">izahid</a>. All Rights Reserved.</p>
+    <a href="https://learn.zone01oujda.ma/git/izahid">izahid</a>. All Rights Reserved.</p>
     `;
+    toggleIcon.addEventListener('click', togglePassword())
 }
 
 // Call the function when the page loads
