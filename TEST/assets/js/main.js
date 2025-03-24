1967460411258eb4ccfe13344194976302df5969
EmanZHD <zahidimane1@gmail.com>
2025-03-24 12:28:04 +0000

    edit login

--- TEST/assets/js/main.js
+++ TEST/assets/js/main.js
@@ -16,3 +16,14 @@ inputs.forEach(input => {
     input.addEventListener("focus", addcl);
     input.addEventListener("blur", remcl);
 });
+function getCurrentYearAsMax() {
+    const currentYear = new Date().getFullYear();
+    const footer = document.querySelector(".footer-content");
+    footer.innerHTML = `
+    <p>&copy;${currentYear} 01Oujda-GraphQL | Crafted with ❤ by
+        <a href="https://learn.zone01oujda.ma/git/izahid">izahid</a>. All Rights Reserved.</p>
+    `;
+}
+
+// Call the function when the page loads
+document.addEventListener("DOMContentLoaded", getCurrentYearAsMax);
